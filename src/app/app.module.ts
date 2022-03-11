d6f6f6c3ca065134687adbad241c581057a7bea9
Methujan <methujan_02@hotmail.com>
2022-03-10 20:29:13 -0500

    generate pokedex-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokedexListComponent } from './components/pokedex-list/pokedex-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokedexListComponent
   ],
   imports: [
     BrowserModule,
