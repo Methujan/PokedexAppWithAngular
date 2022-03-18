92a03a23b7168526a0b79d5bf8892152c8b3462b
Methujan <methujan_02@hotmail.com>
2022-03-17 21:21:31 -0400

    fix component but

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokedexListComponent } from './components/pokedex-list/pokedex-list.component';
-import { PokedexSearchComponent } from './pokedex-search/pokedex-search.component';
+import { PokedexSearchComponent } from './components/pokedex-search/pokedex-search.component';
 
 @NgModule({
   declarations: [
