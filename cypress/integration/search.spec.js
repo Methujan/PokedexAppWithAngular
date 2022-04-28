9e2949a2de19f1b013ea548f785fdbaee868efd8
Methujan <methujan_02@hotmail.com>
2022-04-28 00:11:03 -0400

    cypress test visits homepage

---
+++ cypress/integration/search.spec.js
@@ -0,0 +1,12 @@
+// search.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe("PokeAPI Tests", () => {
+  it("search for Mew", () => {
+    cy.visit("http://localhost:4200/");
+  });
+});
